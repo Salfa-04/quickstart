b653460b77882831a0c4a478f41bf13e0f08ad97
Salfa Chang <me@salfa.cc>
2025-12-07 17:03:51 +0800

    refactor: remove unused IWDG resource from Blinky task

--- {{crate_name}}/src/system/resources.rs
+++ {{crate_name}}/src/system/resources.rs
@@ -9,7 +9,6 @@ use super::private::*;
 assign_resources! {
     /// for `Blinky` task.
     blinky: BlinkySrc {
-        iwdg_p: IWDG,
         led_pin: PC13,
     }
 }
--- {{crate_name}}/src/tasks/blinky/mod.rs
+++ {{crate_name}}/src/tasks/blinky/mod.rs
@@ -2,21 +2,17 @@
 //! # Blinky Task
 //!
 
-use crate::{hal, system::BlinkySrc};
+use crate::{hal, system::*};
 use hal::gpio::{Level, Output as OP, Speed};
-use hal::wdg::IndependentWatchdog as Dog;
 
 #[embassy_executor::task]
 pub async fn task(p: BlinkySrc) -> ! {
     let mut t = utils::init_ticker!(150); // 150ms
 
     let mut led = OP::new(p.led_pin, Level::Low, Speed::Low);
-    let mut dog = Dog::new(p.iwdg_p, 200_000); // 200ms
-
-    // dog.unleash(); // Start the WatchDog
 
     loop {
-        (led.toggle(), dog.pet());
+        led.toggle();
         t.next().await
     }
 }
