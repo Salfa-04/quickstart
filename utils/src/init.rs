b9efe25a86664b724ba6bf562908870ef1d6587f
Salfa Chang <me@salfa.cc>
2025-12-07 16:55:39 +0800

    feat: enhance board selection and configuration for STM32 devices

--- utils/src/init.rs
+++ utils/src/init.rs
@@ -6,6 +6,28 @@ use crate::prelude::{hal, ll};
 use hal::{Config, Peripherals, init, rcc, time::mhz};
 use ll::{Peripherals as CorePeripherals, singleton};
 
+// __pre_init function to be called before main
+core::arch::global_asm! {
+    ".global __pre_init",
+    ".type __pre_init, %function",
+    ".thumb_func",
+    "__pre_init:",
+
+    // // Copy ITCM from FLASH to ITCM
+    // "ldr r0, =__sitcm
+    //  ldr r1, =__eitcm
+    //  ldr r2, =__siitcm
+    //  0:
+    //  cmp r1, r0
+    //  beq 1f
+    //  ldm r2!, {{r3, r4}}
+    //  stm r0!, {{r3, r4}}
+    //  b 0b
+    //  1:",
+
+    "bx lr", // Return from __pre_init
+}
+
 ///
 /// # System Initialization Function
 ///
--- {{crate_name}}/build.rs
+++ {{crate_name}}/build.rs
@@ -18,13 +18,7 @@ fn main() -> std::io::Result<()> {
     // Output the build.map file   : This is useful for analysis.
     cargo_emit::rustc_link_arg!(format!("-Map={}/build.map", package));
 
-    {% if ocd -%}
     write_openocd_config_file(package)?;
-
-    {% else -%}
-    // write_openocd_config_file(package)?;
-
-    {% endif -%}
 
     Ok(())
 }
